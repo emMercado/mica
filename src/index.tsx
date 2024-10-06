b40c24ceda5a799af382d113ce344a7991d37729
u633841 <eemercadobabi@teco.com.ar>
2024-10-06 20:57:41 -0300

    route name

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/<nombre-del-repositorio>">
+    <BrowserRouter basename="/mica">
       <App />
     </BrowserRouter>
   </React.StrictMode>
